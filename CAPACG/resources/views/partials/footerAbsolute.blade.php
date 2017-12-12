387a59b4cfbca12303091eee01f9fe05e65a2811
Adri <adrivar1993@gmail.com>
2017-12-11 23:36:10 -0600

    Fix in FooterAbsolute

--- CAPACG/resources/views/partials/footerAbsolute.blade.php
+++ CAPACG/resources/views/partials/footerAbsolute.blade.php
@@ -1,50 +1,54 @@
 <footer class="blog-footer" id="footerAbsolute">
-	<div id="footerContent" class="container">
-		<div class="row">
-			<div class="col-md-4">
-
-				<p>
-					<span class="copyright">Copyright &copy; CAPACG</span>
-				</p>
-
-				<p>Área de Conservación Guanacaste</p>
-
-				<p>Sistema Nacional de Área de Conservación Costa Rica</p>
-
-				<p>Ministerio de Ambiente y Energía</p>
+<div id="footerContent" class="container">
+	<div class="row">
+		<div class="col-md-4">
 
 
-			</div>
-			<div class="col-md-4">
-				<p>Control de Activos para el Area de Conservación Guanacaste</p>
-				<p>
-					<a id="footerContent" href="#">
-						<i class="fa fa-arrow-up"></i> Arriba</a>
-				</p>
-				<a id="footerContent" href="https://www.facebook.com/ACG.CR">
-					<i class="fa fa-facebook-official"></i>
-				</a>
-				<a id="footerContent" href="https://plus.google.com/u/0/108989919407702501124/posts">
-					<i class="fa fa-google-plus"></i>
-				</a>
-				<a id="footerContent" href="http://www.youtube.com/videosacg">
-					<i class="fa fa-youtube"></i>
-				</a>
-				<a id="footerContent" href="https://www.acguanacaste.ac.cr/noticias?format=feed&type=rss">
-					<i class="fa fa-rss-p"></i>
-				</a>
-				<a id="footerContent" href="https://twitter.com/ACGuanacaste">
-					<i class="fa fa-twitter"></i>
-				</a>
-			</div>
+			<p>Área de Conservación Guanacaste</p>
 
-			<div class="col-md-4">
-				<p>Autores:</p>
-				<p>Omar Miranda Villagra</p>
-				<p>Adriana Vargas Villalobos</p>
-				<p>Cesar Rivera Campos</p>
-			</div>
+			<p>Sistema Nacional de Área de Conservación Costa Rica</p>
+
+			<p>Ministerio de Ambiente y Energía</p>
+			<p>
+				<span class="copyright">Copyright &copy; CAPACG</span>
+			</p>
+
 
 		</div>
+		<div class="col-md-4">
+			
+			
+			<a id="footerContent" href="https://www.facebook.com/ACG.CR">
+				<i class="fa fa-facebook-official"></i>
+			</a>
+			<a id="footerContent" href="https://plus.google.com/u/0/108989919407702501124/posts">
+				<i class="fa fa-google-plus"></i>
+			</a>
+			<a id="footerContent" href="http://www.youtube.com/videosacg">
+				<i class="fa fa-youtube"></i>
+			</a>
+			<a id="footerContent" href="https://www.acguanacaste.ac.cr/noticias?format=feed&type=rss">
+				<i class="fa fa-rss-p"></i>
+			</a>
+			<a id="footerContent" href="https://twitter.com/ACGuanacaste">
+				<i class="fa fa-twitter"></i>
+			</a>
+			<p>
+				<a id="footerContent" href="#">
+				<div></div>
+					<i class="fa fa-arrow-up"></i> Arriba</a>
+			</p>
+
+			<p>Control de Activos para el Área de Conservación Guanacaste</p>
+		</div>
+
+		<div class="col-md-4">
+			<p>Autores:</p>
+			<p>Omar Miranda Villagra</p>
+			<p>Adriana Vargas Villalobos</p>
+			<p>Julio César Rivera Campos</p>
+		</div>
+
 	</div>
+</div>
 </footer>
